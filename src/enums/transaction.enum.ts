04b6e7eda526c5bd31226c6cfa3431676bb0559d
Miko Chu <angelomycar@gmail.com>
2024-06-16 16:47:09 +0800

    RC777-30: add cashback enum

--- src/enums/transaction.enum.ts
+++ src/enums/transaction.enum.ts
@@ -6,6 +6,7 @@ export enum TransactionType {
 export enum TransactionTypeCategory {
   DEPOSIT = 'DEPOSIT',
   REBATE = 'REBATE',
+  CASH_BACK = 'CASH_BACK', // discontinued but still here just in case
   BET_CREDIT = 'BET_CREDIT',
   BET_DEBIT = 'BET_DEBIT',
   REFUND = 'REFUND',
