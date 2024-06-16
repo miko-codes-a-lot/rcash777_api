2323edadff26c74f909f64014539909e4a9d9b86
Miko Chu <angelomycar@gmail.com>
2024-06-17 01:24:10 +0800

    RC777-43: import entities, etc,. in game module

--- src/modules/game/game.module.ts
+++ src/modules/game/game.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { GameService } from './game.service';
 import { GameController } from './game.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Game } from './entities/game.entity';
+import { GameImage } from './entities/game-image.entity';
+import { GameSchedule } from './game.schedule';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Game, GameImage]), HttpModule.register({})],
   controllers: [GameController],
-  providers: [GameService],
+  providers: [GameService, GameSchedule],
 })
 export class GameModule {}
