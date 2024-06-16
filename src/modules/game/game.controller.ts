ea68cbbcb6e51a05105554658f93b5906e375493
Miko Chu <angelomycar@gmail.com>
2024-06-17 01:26:33 +0800

    remove unnecessary game endpoints

--- src/modules/game/game.controller.ts
+++ src/modules/game/game.controller.ts
@@ -1,16 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { GameService } from './game.service';
-import { CreateGameDto } from './dto/create-game.dto';
-import { UpdateGameDto } from './dto/update-game.dto';
 
 @Controller('game')
 export class GameController {
   constructor(private readonly gameService: GameService) {}
-
-  @Post()
-  create(@Body() createGameDto: CreateGameDto) {
-    return this.gameService.create(createGameDto);
-  }
 
   @Get()
   findAll() {
@@ -20,15 +13,5 @@ export class GameController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.gameService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGameDto: UpdateGameDto) {
-    return this.gameService.update(+id, updateGameDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.gameService.remove(+id);
   }
 }
