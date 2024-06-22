dcc8b2192c8fa96949f47eab7bdbd71ab473a043
Miko Chu <angelomycar@gmail.com>
2024-06-23 00:05:23 +0800

    add auth endpoint contract

--- src/modules/wallet/wallet.controller.ts
+++ src/modules/wallet/wallet.controller.ts
@@ -1,34 +1,41 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
 import { WalletService } from './wallet.service';
-import { CreateWalletDto } from './dto/create-wallet.dto';
-import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { Response } from 'express';
+import { EResponse } from 'src/enums/response.enum';
 
 @Controller('wallet')
 export class WalletController {
   constructor(private readonly walletService: WalletService) {}
 
-  @Post()
-  create(@Body() createWalletDto: CreateWalletDto) {
-    return this.walletService.create(createWalletDto);
-  }
+  @Post('authenticate')
+  async authenticate(@Res() res: Response) {
+    const no = Math.random();
+    if (no === 1) {
+      res.status(EResponse.NOTFOUND).json({
+        error: {
+          errorCode: 'TOKEN_NOT_FOUND',
+          errorMessage: 'Token is already validated or invalid',
+        },
+      });
+    } else if (no === 2) {
+      res.status(EResponse.NOTFOUND).json({
+        error: {
+          errorCode: 'GAME_NOT_FOUND',
+          errorMessage: 'Game not found',
+        },
+      });
+    }
 
-  @Get()
-  findAll() {
-    return this.walletService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.walletService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWalletDto: UpdateWalletDto) {
-    return this.walletService.update(+id, updateWalletDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.walletService.remove(+id);
+    return {
+      client: 'CASINO1',
+      currency: 'USD',
+      testAccount: 'false',
+      country: 'JP',
+      affiliate: 'aff-1',
+      displayName: 'CasinoPlayer123',
+      jurisdiction: '',
+      player: 'Player1',
+      balance: 1000,
+    };
   }
 }
