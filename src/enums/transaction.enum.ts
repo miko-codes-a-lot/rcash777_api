7fc83efc77873a9173167fb5c3e5003710ef80b1
Miko Chu <angelomycar@gmail.com>
2024-06-24 09:24:48 +0800

    RC777-54: modify transaction enum to be compatible with providers

--- src/enums/transaction.enum.ts
+++ src/enums/transaction.enum.ts
@@ -6,9 +6,19 @@ export enum TransactionType {
 export enum TransactionTypeCategory {
   DEPOSIT = 'DEPOSIT',
   REBATE = 'REBATE',
-  CASH_BACK = 'CASH_BACK', // discontinued but still here just in case
-  BET_CREDIT = 'BET_CREDIT',
-  BET_DEBIT = 'BET_DEBIT',
   REFUND = 'REFUND',
+  CASH_BACK = 'CASH_BACK', // discontinued but still here just in case
+  BET_DEBIT = 'BET_DEBIT',
+  WIN = 'WIN',
+  PAYOUT = 'PAYOUT',
+  BET_CREDIT = 'BET',
+  TIP = 'TIP',
+  PARTIAL_REFUND = 'PARTIAL_REFUND',
+  BUY_IN = 'BUY_IN',
+  CASH_OUT = 'CASH_OUT',
+  FREEROUND_WIN = 'FREEROUND_WIN',
+  TOURNAMENT_WIN = 'TOURNAMENT_WIN',
+  CAMPAIGN_WIN = 'CAMPAIGN_WIN',
+  FREEGAME_WIN = 'FREEGAME_WIN',
   CONFISCATE = 'CONFISCATE',
 }
