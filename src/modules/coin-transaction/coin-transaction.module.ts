f0848a6474633f21f7f1fd68cd946bca93bf4a53
Miko Chu <angelomycar@gmail.com>
2024-07-05 05:01:01 +0800

    RC777-22: register request coin entity in coin transaction module

--- src/modules/coin-transaction/coin-transaction.module.ts
+++ src/modules/coin-transaction/coin-transaction.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../user/entities/user.entity';
 import { CashTransaction } from '../cash-transaction/entities/cash-transaction.entity';
 import { CoinTransaction } from './entities/coin-transaction.entity';
+import { CoinRequest } from './entities/coin-request.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, CashTransaction, CoinTransaction])],
+  imports: [TypeOrmModule.forFeature([User, CashTransaction, CoinTransaction, CoinRequest])],
   controllers: [CoinTransactionController],
   providers: [CoinTransactionService],
   exports: [CoinTransactionService],
