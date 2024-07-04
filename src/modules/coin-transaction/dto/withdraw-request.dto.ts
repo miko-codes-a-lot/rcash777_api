66129d600f44707dd471fb1deaf79747f6c985ff
Miko Chu <angelomycar@gmail.com>
2024-07-05 05:22:18 +0800

    RC777-67: rename request coin transaction dto

--- src/modules/coin-transaction/dto/withdraw-request.dto.ts
+++
@@ -1,9 +0,0 @@
-import Joi from 'joi';
-
-export const WithdrawRequestSchema = Joi.object({
-  amount: Joi.number().min(100).max(50000).required(),
-});
-
-export class WithdrawRequestDTO {
-  amount: number;
-}
