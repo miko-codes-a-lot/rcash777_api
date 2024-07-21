8951040c297f38ecc63e3377e2d32fd7efb3b9f0
Miko Chu <angelomycar@gmail.com>
2024-07-21 14:28:17 +0800

    RC777-24: add find transaction with date range query & array type & find one; based on user tree

--- src/modules/commission/commission.controller.ts
+++ src/modules/commission/commission.controller.ts
@@ -1,34 +1,24 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { CommissionService } from './commission.service';
-import { CreateCommissionDto } from './dto/create-commission.dto';
-import { UpdateCommissionDto } from './dto/update-commission.dto';
+import { CommissionPaginateDTO } from 'src/schemas/paginate-query.dto';
+import { AuthRequired } from 'src/decorators/auth-required.decorator';
+import { AuthIsNot } from 'src/decorators/auth-is-not';
+import { RequestUser } from 'src/decorators/request-user.decorator';
+import { User } from '../user/entities/user.entity';
 
+@AuthRequired()
+@AuthIsNot(['isPlayer'])
 @Controller('commission')
 export class CommissionController {
   constructor(private readonly commissionService: CommissionService) {}
 
-  @Post()
-  create(@Body() createCommissionDto: CreateCommissionDto) {
-    return this.commissionService.create(createCommissionDto);
-  }
-
   @Get()
-  findAll() {
-    return this.commissionService.findAll();
+  findAll(@RequestUser() user: User, @Query() query: CommissionPaginateDTO) {
+    return this.commissionService.findAll(user, query);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commissionService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommissionDto: UpdateCommissionDto) {
-    return this.commissionService.update(+id, updateCommissionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commissionService.remove(+id);
+  findOne(@RequestUser() user: User, @Param('id') id: string) {
+    return this.commissionService.findOne(user, id);
   }
 }
--- src/modules/commission/commission.service.ts
+++ src/modules/commission/commission.service.ts
@@ -1,26 +1,87 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCommissionDto } from './dto/create-commission.dto';
-import { UpdateCommissionDto } from './dto/update-commission.dto';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository, TreeRepository } from 'typeorm';
+import { CommissionPaginateDTO } from 'src/schemas/paginate-query.dto';
+import { CommissionPool } from './entities/commission-pool.entity';
+import { User } from '../user/entities/user.entity';
+import { Commission } from './entities/commission.entity';
 
 @Injectable()
 export class CommissionService {
-  create(createCommissionDto: CreateCommissionDto) {
-    return 'This action adds a new commission';
+  private treeUserRepo: TreeRepository<User>;
+
+  constructor(
+    @InjectRepository(CommissionPool)
+    private readonly poolRepo: Repository<CommissionPool>,
+
+    readonly dataSource: DataSource,
+  ) {
+    this.treeUserRepo = dataSource.manager.getTreeRepository(User);
   }
 
-  findAll() {
-    return `This action returns all commission`;
+  private async _getUserChildrenIds(user: User) {
+    const children = await this.treeUserRepo.findDescendants(user);
+    return children.map((u) => u.id);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} commission`;
+  async findAll(user: User, query: CommissionPaginateDTO) {
+    const { page, pageSize, sortOrder, types, startDate, endDate } = query;
+
+    const descendants = await this._getUserChildrenIds(user);
+
+    const [tx, count] = await this.poolRepo
+      .createQueryBuilder('commissionPool')
+      .innerJoinAndSelect('commissionPool.commissions', 'commission')
+      .innerJoinAndSelect('commissionPool.player', 'player')
+      .where((qb) => {
+        const subQuery = qb
+          .subQuery()
+          .select('1')
+          .from(Commission, 'subCommission')
+          .where('subCommission.pool = commissionPool.id')
+          .andWhere('subCommission.user_player_id IN (:...descendants)', { descendants })
+          .getQuery();
+        return 'EXISTS (' + subQuery + ')';
+      })
+      .andWhere('commissionPool.type IN (:...types)', { types })
+      .andWhere('commissionPool.createdAt BETWEEN :startDate AND :endDate', { startDate, endDate })
+      .orderBy('commissionPool.createdAt', sortOrder)
+      .skip((page - 1) * pageSize)
+      .take(pageSize)
+      .getManyAndCount();
+
+    return {
+      total: count,
+      totalPages: Math.ceil(count / pageSize),
+      page,
+      pageSize,
+      items: tx,
+    };
   }
 
-  update(id: number, updateCommissionDto: UpdateCommissionDto) {
-    return `This action updates a #${id} commission`;
-  }
+  async findOne(user: User, id: string) {
+    const descendants = await this._getUserChildrenIds(user);
+    const pool = await this.poolRepo
+      .createQueryBuilder('commissionPool')
+      .innerJoinAndSelect('commissionPool.commissions', 'commission')
+      .innerJoinAndSelect('commissionPool.player', 'player')
+      .where('commissionPool.id = :id', { id })
+      .andWhere((qb) => {
+        const subQuery = qb
+          .subQuery()
+          .select('1')
+          .from(Commission, 'subCommission')
+          .where('subCommission.pool = commissionPool.id')
+          .andWhere('subCommission.user_player_id IN (:...descendants)', { descendants })
+          .getQuery();
+        return 'EXISTS (' + subQuery + ')';
+      })
+      .getOne();
 
-  remove(id: number) {
-    return `This action removes a #${id} commission`;
+    if (!pool) {
+      throw new BadRequestException('Commission not found');
+    }
+
+    return pool;
   }
 }
