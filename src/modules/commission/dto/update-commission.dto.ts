bcf7913c22a8db7a8173b7a0d3293296808a3dbf
Miko Chu <angelomycar@gmail.com>
2024-07-21 14:27:38 +0800

    RC777-24: remove commission create & update dto

--- src/modules/commission/dto/update-commission.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCommissionDto } from './create-commission.dto';
-
-export class UpdateCommissionDto extends PartialType(CreateCommissionDto) {}
