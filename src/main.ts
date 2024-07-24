7722b25b22d14d6fe88317b43250e93335843976
Miko Chu <angelomycar@gmail.com>
2024-07-25 01:36:04 +0800

    add class-validator class-transformer

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 import config from './config/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('RCash777 API')
