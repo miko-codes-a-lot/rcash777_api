b9b5a9f4da565155d449d172bdf3da313fb46eb4
Miko Chu <angelomycar@gmail.com>
2024-07-25 01:41:37 +0800

    explicitly transfer data type to defined dto

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
 
   const swaggerConfig = new DocumentBuilder()
     .setTitle('RCash777 API')
