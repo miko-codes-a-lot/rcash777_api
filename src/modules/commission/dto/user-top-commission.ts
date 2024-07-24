03366064868f84f1cbfad9d56b5fe2f89c312b76
Miko Chu <angelomycar@gmail.com>
2024-07-25 01:41:46 +0800

    add top validation max min

--- src/modules/commission/dto/user-top-commission.ts
+++ src/modules/commission/dto/user-top-commission.ts
@@ -1,8 +1,10 @@
-import { IsEnum, IsNotEmpty, IsNumberString } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsNumber, IsNumberString, Max, Min } from 'class-validator';
 import { UserRole } from 'src/enums/user-role.enum';
 
 export class UserTopCommissionDTO {
-  @IsNumberString()
+  @IsNumber()
+  @Max(20)
+  @Min(5)
   top: number = 10;
 
   @IsNotEmpty()
