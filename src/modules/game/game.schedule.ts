c95708c9567facd04f0375f0b456ba4ba83705f0
Miko Chu <angelomycar@gmail.com>
2024-07-30 21:07:27 +0800

    game schedule add retrieval of provider list

--- src/modules/game/game.schedule.ts
+++ src/modules/game/game.schedule.ts
@@ -4,6 +4,8 @@ import { GameService } from './game.service';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { GameDTO } from './dto/game.dto';
 import config from '../../config/config';
+import { zip } from 'rxjs';
+import { ProviderDTO } from './dto/provider.dto';
 
 const NEXTRAL_URI = config.game_api.zenith.uri;
 
@@ -20,20 +22,36 @@ export class GameSchedule {
     name: 'get_games',
     timeZone: 'Asia/Singapore',
   })
-  performTask() {
+  async performTask() {
     this.logger.debug('Retrieving games in cron!');
-    return this.http
-      .get(`${NEXTRAL_URI}/public/v1/gameList`, {
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Basic ${config.game_api.zenith.operation.basic}`,
-        },
-      })
-      .subscribe({
-        next: async (response) => {
-          const games: GameDTO[] = response.data;
-          await this.gameService.createMany(games);
-        },
-      });
+    return zip([
+      this.http
+        .get(`${NEXTRAL_URI}/lobby/v1/clients`, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ${config.game_api.zenith.operation.basic}`,
+          },
+        })
+        .subscribe({
+          next: async (response) => {
+            const providers: ProviderDTO[] = response.data;
+            await this.gameService.createManyProviders(providers);
+          },
+          error: (err) => console.error(err, 'error'),
+        }),
+      this.http
+        .get(`${NEXTRAL_URI}/public/v1/gameList`, {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ${config.game_api.zenith.operation.basic}`,
+          },
+        })
+        .subscribe({
+          next: async (response) => {
+            const games: GameDTO[] = response.data;
+            await this.gameService.createMany(games);
+          },
+        }),
+    ]);
   }
 }
